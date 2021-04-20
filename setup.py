30518888e373f428ade70e4423941af14f9470d0
Daniel McKnight <daniel@neongecko.com>
2021-04-20 14:30:14 -0700

    Update setup to reference requirements.txt, version.py, README.md Use ovos_plugin_manager for locating and loading modules

--- setup.py
+++ setup.py
@@ -1,23 +1,32 @@
 from setuptools import setup
 
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+with open("./version.py", "r", encoding="utf-8") as v:
+    for line in v.readlines():
+        if line.startswith("__version__"):
+            if '"' in line:
+                version = line.split('"')[1]
+            else:
+                version = line.split("'")[1]
+
+with open("./requirements.txt", "r", encoding="utf-8") as r:
+    requirements = r.readlines()
+
 setup(
     name='neon_speech',
-    version='0.1.3',
+    version=version,
     packages=['neon_speech', 'neon_speech.plugins',
               'neon_speech.plugins.modules',
               'neon_speech.plugins.modules.background',
               'neon_speech.plugins.modules.audio_normalizer'],
-    url='https://github.com/NeonJarbas/neon_speech',
-    license='',
-    install_requires=["requests",
-                      "pyaudio",
-                      "mycroft-messagebus-client>=0.8.4",
-                      "SpeechRecognition==3.8.1",
-                      "ovos_utils",
-                      "pydub"],
-    author='jarbasAi',
-    author_email='jarbasai@mailfence.com',
-    description='speech client for Neon',
+    url='https://github.com/NeonGeckoCom/neon_speech',
+    license='NeonAI License v1.0',
+    install_requires=requirements,
+    author='Neongecko',
+    author_email='developers@neon.ai',
+    description=long_description,
     entry_points={
         'console_scripts': [
             'neon_speech_client=neon_speech.__main__:main'
