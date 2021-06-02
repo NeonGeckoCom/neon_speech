6ef88789c38ff062af53892ac9ff4a709d03a9b4
Daniel McKnight <daniel@neongecko.com>
2021-06-02 15:34:31 -0700

    Replace HotWordFactory override for config support

--- neon_speech/hotword_factory.py
+++ neon_speech/hotword_factory.py
@@ -19,4 +19,58 @@
 # WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
 # USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+from threading import Event
+
 from mycroft.client.speech.hotword_factory import *
+
+
+class HotWordFactory:
+    CLASSES = {
+        # engines without plugins can be added here
+    }
+
+    @staticmethod
+    def load_module(module, hotword, config, lang, loop):
+        LOG.info('Loading "{}" wake word via {}'.format(hotword, module))
+        instance = None
+        complete = Event()
+
+        def initialize():
+            nonlocal instance, complete
+            try:
+                if module in HotWordFactory.CLASSES:
+                    clazz = HotWordFactory.CLASSES[module]
+                else:
+                    clazz = load_wake_word_plugin(module)
+                    LOG.info('Loaded the Wake Word plugin {}'.format(module))
+
+                instance = clazz(hotword, config, lang=lang)
+            except TriggerReload:
+                complete.set()
+                sleep(0.5)
+                loop.reload()
+            except NoModelAvailable:
+                LOG.warning('Could not found find model for {} on {}.'.format(
+                    hotword, module
+                ))
+                instance = None
+            except Exception:
+                LOG.exception(
+                    'Could not create hotword. Falling back to default.')
+                instance = None
+            complete.set()
+
+        Thread(target=initialize, daemon=True).start()
+        if not complete.wait(INIT_TIMEOUT):
+            LOG.info('{} is taking too long to load'.format(module))
+            complete.set()
+        return instance
+
+    @classmethod
+    def create_hotword(cls, hotword="dummy", config=None,
+                       lang="en-us", loop=None):
+        ww_config_core = config or {}
+        config = ww_config_core.get(hotword) or {}
+        module = config.get("module", "dummy_ww_plug")
+        return cls.load_module(module, hotword, config, lang, loop) or \
+            HotWordEngine("dummy")
