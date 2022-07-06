NeonGeckoCom/neon_audio

Install default plugins to images for startup speedup

Dockerfile
@@ -42,4 +42,6 @@ RUN pip install wheel && \
 COPY docker_overlay/ /
 RUN chmod ugo+x /root/run.sh
 
+RUN neon-audio install-plugin -f
+
 CMD ["/root/run.sh"]

NeonGeckoCom/neon_speech

Dockerfile
@@ -28,4 +28,6 @@ RUN pip install wheel && \
 COPY docker_overlay/ /
 RUN chmod ugo+x /root/run.sh
 
+RUN neon-speech install-plugin -f
+
 CMD ["/root/run.sh"]